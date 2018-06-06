b7e53f557f8ced85e81ab47a8da4632a1c3ae2f6
johnnyn1261 <johnnyn1261@gmail.com>
2018-06-06 00:01:02 -0400

    Added audio for each word

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,16 +7,19 @@ public class Word {
     private String defaultTranslation;
     private String miwokTranslation;
     private int imageResourceID = -1;
+    private int audioResourceID;
 
-    public Word (String defaultTranslation, String miwokTranslation) {
+    public Word (String defaultTranslation, String miwokTranslation, int audioResourceID) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
+        this.audioResourceID = audioResourceID;
     }
 
-    public Word (String defaultTranslation, String miwokTranslation, int imageResourceID) {
+    public Word (String defaultTranslation, String miwokTranslation, int imageResourceID, int audioResourceID) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
         this.imageResourceID = imageResourceID;
+        this.audioResourceID = audioResourceID;
     }
 
     public String getDefaultTranslation() {
@@ -33,5 +36,9 @@ public class Word {
 
     public boolean hasImage() {
         return (imageResourceID == -1) ? false : true;
+    }
+
+    public int getAudioResourceID() {
+        return audioResourceID;
     }
 }
